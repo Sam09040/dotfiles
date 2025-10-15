c9f70973df4d0c9b64601eaeb36b4821afbd26ca
sam09040 <jpalmeida0904@gmail.com>
2025-10-15 15:59:48 -0300

    Consertando os scripts

--- backup.sh
+++ backup.sh
@@ -6,6 +6,7 @@ cp -r ~/.config/nvim/ ~/dotfiles/.config/
 cp -r ~/.config/ghostty/ ~/dotfiles/.config/
 cp -r ~/.config/yazi/ ~/dotfiles/.config/
 cp -r ~/.zshrc ~/dotfiles/zsh/
+cp -r ~/.p10k.zsh ~/dotfiles/zsh/
 cp -r ~/bat/ ~/dotfiles
 cp -r ~/.tmux.conf ~/dotfiles
 
--- sync.sh
+++ sync.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "Starting syncronization"
-cp -r ~/dotfiles/.config ~/.config/
+cp -r ~/dotfiles ~/
+cp -r ~/dotfiles/.config/ ~/.config/
 cp -r ~/dotfiles/bat/ ~/
-cp -r ~/dotfiles/.tmux ~/
+cp -r ~/dotfiles/.tmux/ ~/
 cp -r ~/dotfiles/.tmux.conf ~/
+cp -r ~/dotfiles/zsh/ ~/
 echo "Finished syncronization"
