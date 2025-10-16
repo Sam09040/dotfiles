15d7852797189f53a1051914007dff0a5b709350
sam09040 <jpalmeida0904@gmail.com>
2025-10-16 19:09:41 -0300

    updating scripts

--- full_backup.sh
+++ full_backup.sh
@@ -3,7 +3,11 @@
 echo "Starting copies"
 
 cp -r ~/.config/nvim/ ~/dotfiles/.config/
+cp -r ~/.config/ghostty/ ~/dotfiles/.config/
+cp -r ~/.config/yazi/ ~/dotfiles/.config/
 cp -r ~/.zshrc ~/dotfiles/zsh/
+cp -r ~/.p10k.zsh ~/dotfiles/zsh/
+cp -r ~/bat/ ~/dotfiles
 cp -r ~/.tmux.conf ~/dotfiles
 
 echo "Finished copies"
--- full_sync.sh
+++ full_sync.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "Starting syncronization"
-cp -r ~/dotfiles/.config/nvim/ ~/.config/
+cp -r ~/dotfiles ~/
+cp -r ~/dotfiles/.config/ ~/.config/
+cp -r ~/dotfiles/bat/ ~/
+cp -r ~/dotfiles/.tmux/ ~/
 cp -r ~/dotfiles/.tmux.conf ~/
 cp -r ~/dotfiles/zsh/ ~/
 echo "Finished syncronization"
