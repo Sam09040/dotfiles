5a361b0a5935598b47dd17881c717c27667dd5ad
sam09040 <jpalmeida0904@gmail.com>
2025-10-20 15:18:50 -0300

    correction on the sync scripts

--- full_sync.sh
+++ full_sync.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo "Starting syncronization"
-cp -r ~/dotfiles ~/
 cp -r ~/dotfiles/.config/ ~/.config/
 cp -r ~/dotfiles/bat/ ~/
 cp -r ~/dotfiles/.tmux/ ~/
 cp -r ~/dotfiles/.tmux.conf ~/
-cp -r ~/dotfiles/zsh/ ~/
+cp -r ~/dotfiles/zsh/* ~/
 echo "Finished syncronization"
--- sync.sh
+++ sync.sh
@@ -3,5 +3,5 @@
 echo "Starting syncronization"
 cp -r ~/dotfiles/.config/nvim/ ~/.config/
 cp -r ~/dotfiles/.tmux.conf ~/
-cp -r ~/dotfiles/zsh/ ~/
+cp -r ~/dotfiles/zsh/.zshrc ~/
 echo "Finished syncronization"
